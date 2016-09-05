ff96877f0925f2fbb1e43a74b6f3082adfc2f8f7
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-09-05 19:18:52 -0400

    Add files via upload

---
+++ Day 2-Operators.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        double mealTip = (mealCost * tipPercent)/100;
+        double mealTax = (mealCost * taxPercent)/100;
+        mealCost += mealTip + mealTax;
+        
+      
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(mealCost);
+        System.out.println("The total meal cost is " + totalCost +" dollars.");
+        // Print your result 
+    }
+}
