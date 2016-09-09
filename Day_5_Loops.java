99def948bc17fa82a50d2f7853d48517d61ff2bb
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-09-09 17:03:01 -0400

    Update Day_5_Loops.java

--- Day_5_Loops.java
+++ Day_5_Loops.java
@@ -6,7 +6,8 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         for(int i=1;i<=10;i++)
