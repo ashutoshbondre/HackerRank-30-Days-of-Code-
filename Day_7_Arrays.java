31f0c26db1c6ef42dd13fcdab3eb106ed33aa993
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-09-12 17:26:57 -0400

    Update Day_7_Arrays.java

--- Day_7_Arrays.java
+++ Day_7_Arrays.java
@@ -8,7 +8,8 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
-        for(int i=0; i < n; i++){
+        for(int i=0; i < n; i++)
+        {
             arr[i] = in.nextInt();
         }
         in.close();
