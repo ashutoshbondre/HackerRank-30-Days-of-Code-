eeed854e249b237ffead8728d47342cd1c5c8691
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-09-24 20:55:03 -0400

    Add files via upload

---
+++ Day_14_Scope.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
