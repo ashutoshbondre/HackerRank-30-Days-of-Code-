36fcaef0babb09e65f6fc638ecd06989bb37c799
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-09-25 22:55:04 -0400

    Add files via upload

---
+++ Day_15_LinkedList.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+public static  Node insert(Node head,int data) {
+        //Complete this method
+    }
