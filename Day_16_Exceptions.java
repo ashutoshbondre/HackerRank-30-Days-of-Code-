eb856e5631dc5a8409b9cdc3c8af2f4503e9199f
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-09-27 20:53:55 -0400

    Update Day_16_Exceptions.java

--- Day_16_Exceptions.java
+++ Day_16_Exceptions.java
@@ -6,7 +6,8 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner in = new Scanner(System.in);
         String S = in.next();
     }
