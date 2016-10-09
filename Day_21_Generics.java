3b0f188ee502b762c9a48d47aaf9cbe119fa0685
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-10-08 23:24:06 -0400

    Add files via upload

---
+++ Day_21_Generics.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Method;
+
+class Solution {
+	
+	 public static void main(String args[]){
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = { "Hello", "World" };
+        
+        printArray( intArray  );
+        printArray( stringArray );
+        
+        if(Solution.class.getDeclaredMethods().length > 2){
+            System.out.println("You should only have 1 method named printArray.");
+        }
+    }
+}
