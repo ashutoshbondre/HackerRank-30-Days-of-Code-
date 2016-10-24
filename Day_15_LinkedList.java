86febf95fa64746da5528cccff1426638df2d78f
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-10-24 00:45:56 -0400

    Update Day_15_LinkedList.java

--- Day_15_LinkedList.java
+++ Day_15_LinkedList.java
@@ -1,20 +1,54 @@
 import java.io.*;
 import java.util.*;
 
-class Node 
-    {
+class Node {
 	int data;
 	Node next;
-	Node(int d) 
-	{
-        	data = d;
-        	next = null;
-        }
+	Node(int d) {
+        data = d;
+        next = null;
+    }
 }
 
 class Solution {
-public static  Node insert(Node head,int data)
-    {
-        //Complete this method 
-	    Node x=new Node(3);
+	 public static  Node insert(Node head,int data) {
+        //Complete this method
+        Node top = head;
+        if(top==null)
+            {
+                    top = new Node(data); 
+                    head=top;
+            }
+        
+        else
+        {
+            
+        
+            while(top.next!=null)
+            top=top.next;
+            top.next= new Node(data);
+        }
+        
+           return head;
     }
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
