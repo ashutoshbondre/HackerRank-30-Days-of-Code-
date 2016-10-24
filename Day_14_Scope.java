63c2cf622b3eb6455e2307fce662e578b45ba6f0
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-10-23 22:51:25 -0400

    Update Day_14_Scope.java

--- Day_14_Scope.java
+++ Day_14_Scope.java
@@ -12,21 +12,21 @@ class Difference
     // Add your code here
     public Difference (int a[])
         {
-        elements=a;
+            elements=a;
         }
     public void computeDifference()
         {
             maximumDifference=0;
-        for(int i=0;i<elements.length-1;i++)
+            for(int i=0;i<elements.length-1;i++)
                 {
                 for(int j=i+1;j<elements.length;j++)
                     {
                     
-                    int value= Math.abs(elements[i]-elements[j]);
-                    if (value>maximumDifference)
-                        {
+                        int value= Math.abs(elements[i]-elements[j]);
+                        if (value>maximumDifference)
+                          {
                             maximumDifference=value;
-                        }
+                          }
                     }
                 }
         }
