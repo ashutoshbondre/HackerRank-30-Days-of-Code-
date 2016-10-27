7140b1991672c43aad2c88f43f9b781b58dd8106
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-10-26 23:24:37 -0400

    Update Day_17_MoreExceptions.java

--- Day_17_MoreExceptions.java
+++ Day_17_MoreExceptions.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Calculator 
   {
     public int power(int a,int b) throws Exception
-        {
+       {
         if(a<0||b<0)
             {
             throw new Exception("n and p should be non-negative");
