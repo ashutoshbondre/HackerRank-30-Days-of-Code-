4602f1a2a1f2eb21a2805ea9a08d2f071f89f5a4
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-10-26 23:23:36 -0400

    Update Day_17_MoreExceptions.java

--- Day_17_MoreExceptions.java
+++ Day_17_MoreExceptions.java
@@ -1,16 +1,40 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.io.*;
+//Write your code here
+class Calculator 
+  {
+    public int power(int a,int b) throws Exception
+        {
+        if(a<0||b<0)
+            {
+            throw new Exception("n and p should be non-negative");
+        }
+        int result = (int)(Math.pow(a,b));
+        return result;
+    }
+  }
+class Solution{
 
-public class Solution {
-
-    public static void main(String[] args)
-    
+    public static void main(String []argh)
     {
         Scanner in = new Scanner(System.in);
-        String S = in.next();
-        System.out.println("Is it right input");
+        int T=in.nextInt();
+        while(T-->0)
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try
+            {
+                int ans=myCalculator.power(n,p);
+                System.out.println(ans);
+                
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 }
