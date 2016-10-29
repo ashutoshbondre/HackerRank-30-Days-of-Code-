b4c6d04ac0321ba32f11af6080c1838d4e2f4942
Ashutosh Bondre <ashutoshbondre@gmail.com>
2016-10-28 23:08:13 -0400

    Add files via upload

---
+++ Day_23_BST Level Order.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+    
